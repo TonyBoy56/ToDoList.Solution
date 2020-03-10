041ff7a91987a41580fc3569f963394084a1aeac
Antonio Cruz <codrocks.cruz3@gmai.com>
2020-03-10 07:18:49 -0700

    add notes for CreateForm.cshtml and HomeController.cs.

--- ToDoList/Contollers/HomeContoller.cs
+++ ToDoList/Contollers/HomeContoller.cs
@@ -11,5 +11,19 @@ namespace ToDoList.Controllers
       Item startItem = new Item("Add first item to To Do List");
       return View(startItem);
     }
+
+    [Route("/items/new")]
+    public ActionResult CreateForm()
+    {
+      return View();
+    }
+    // Tells our controller what to do when the user submits the form. //
+    // View method is using overloaded constructors under the hood. This makes it capable of accepting as many arguments as we need. //
+    [Route("/items")]
+    public ActionResult Create(string description)
+    {
+      Items myItem = new Item(description);
+      return View("Index", myItem);
+    }
   }
 }
